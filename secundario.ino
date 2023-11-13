d69b57abce447e56cc3fffcf154949f5ef4fd9ef
NicolasSpadetto <86129253+NicolasSpadetto@users.noreply.github.com>
2023-11-13 13:33:43 -0300

    Add files via upload

--- secundario.ino
+++ secundario.ino
@@ -1,3 +1,4 @@
+
 const byte start_end_Flag = 0x7E;
 
 //endereços - 4bits----------------------------------
@@ -15,7 +16,7 @@ int ACK2 = 0;
 */
 
 //buffers de dados--------------------------------
-String buffer[2] = {"1100", "1101"};
+String buffer[2] = {"110000", "1100111"};
 String ult_recebido;
 
 /*
@@ -30,12 +31,18 @@ String DIV = "1000100000010001";
 String crcGen(String entrada, String DIV)
 {
     int div_totais = entrada.length();
-    String crc;
+    int DIV_mag = DIV.length();
+    String crc = "";
 
-    for (int i = 0; i < (DIV.length() - 1); i++) entrada+= '0';
-    for (int i = 0; i < div_totais; i++) for (int j = 0; j < DIV.length(); j++) entrada[i + j] = (entrada[i + j] != DIV[j]);
+    for (int i = 0; i < (DIV_mag - 1); i++) entrada += '0';
+ 
+    for (int i = 0; i < div_totais; i++) for (int j = 0; j < DIV_mag; j++) 
+    {
+      if (entrada[i + j] != DIV[j]) entrada[i + j] = '1';
+      else entrada[i + j] = '0';
+    }
 
-    for (int i = (entrada.length() - 1); i < DIV.length(); i++) crc += entrada[i];
+    for (int i = (div_totais - 1); i < entrada.length(); i++) crc += entrada[i];
 
     return crc;
 }
@@ -43,34 +50,36 @@ String crcGen(String entrada, String DIV)
 int strToNbr(String dado)
 {
   int out = 0;
-  int pow2 = 1;
-  for (int i = 0; i < dado.length(); i++) 
+  int pow2;
+  int mag = dado.length();
+  
+  for (int i = 0; i < mag; i++) 
   {
-    pow2 = 1;
+    pow2 = 0;
     
-    for (int j = 0; j < (dado.length() - 1); j++)
+    if (dado[i] == '1') 
     {
-      pow2 *= 2;
+      pow2 = 1;
+      for (int j = 0; j < (mag - i - 1); j++) pow2 *= 2;
     }
-
-    out += pow2;
+    
+    out += pow2;    
   }
-  if (dado[dado.length() - 1]) out += 1;
-
+  
   return out;
 }
 
 String NbrToBitS(int entrada, int tamanho = 4)
 {
   String ent_clone = String(entrada, BIN);
-  String out;
+  String out = "";
 
-  for (int i = (tamanho - 1); i >= 0; i++) out += '0';
+  for (int i = 0; i < tamanho; i++) out += '0';
 
-  int dif_tamanho = tamanho - ent_clone.length();
+  int dif = tamanho - ent_clone.length();
 
-  for (int i = (ent_clone.length() - 1); i >= 0; i++) out[i] = ent_clone[i - dif_tamanho];
-
+  for (int i = (ent_clone.length() - 1); i >= 0; i--) out[i + dif] = ent_clone[i];
+  
   return out;
 }
 
@@ -114,7 +123,7 @@ int com_byte;
 String bts_clone;
 int AUX;
 String AUX1;
-bool alternar = 0; //0: buffer 1 manda|| 1: buffer 2
+
 int CRC = 0;// 0 não pede outro frame, 1 pede pra o principal
 
 
@@ -128,32 +137,36 @@ void setup()
 
 void loop()
 {
-  bts_clone = "";
+  delay(1000);
 
   if (RX0_TX1) //fala---------------------------------------------------------
   {
     if (CRC == 0)
     {
+      
       Serial.write(start_end_Flag);
 
 
       //ADDRESS---------------------
-    
+      
+      bts_clone = p_addr;
       bts_clone += my_addr;
-      bts_clone += p_addr;
+      
       com_byte = strToNbr(bts_clone);
-      bts_clone = "";
-
+      
+      
       Serial.write(com_byte);//<--ADDRESS
 
       //CONTROLL---------------------
     
-      bts_clone += '0';
+      bts_clone = '0';
       bts_clone += NbrToBitS(NS, 3);
       bts_clone += '0';
       bts_clone += NbrToBitS(ACK, 3);
       com_byte = strToNbr(bts_clone);
-      bts_clone = "";
+      
+      
+
 
       Serial.write(com_byte);//<--CONTROLL
 
@@ -161,20 +174,24 @@ void loop()
 
       //NS par envia [0], NS ímpar
 
-      if (NS/2)
+      if ((NS % 2) == 0)
       {
         com_byte = strToNbr(buffer[0]);
         bts_clone = buffer[0];
+        
       }
       else
       { 
         com_byte = strToNbr(buffer[1]);
         bts_clone = buffer[1];
+        
       }
+      NS++;
+      
         
       //CRC---------------------
       com_byte = strToNbr(crcGen(bts_clone, DIV));
-
+      
       Serial.write(com_byte); //<--CRC
 
       Serial.write(start_end_Flag);
@@ -208,53 +225,63 @@ void loop()
     }
     
     RX0_TX1 = !RX0_TX1;
-    bts_clone = "";
+    
   }
-  else if((Serial.available() > 0) && (!RX0_TX1))//ouve--------------------------------------------
+  else if(Serial.available() > 0)//ouve--------------------------------------------
   {    
+    delay(2000);
+    //Serial.println("meu deus foi");
     com_byte = Serial.read();
 
     if (com_byte == start_end_Flag)
     {
-      delay(1000);
-
       //ADDRESS---------------------
+      //Serial.println("meu deus foi");
 
+      delay(1000);
       com_byte = Serial.read();
       delay(1000);
-      if (bitRead(com_byte, 4) == strToNbr(my_addr))// tem q trocar caso mude o secundário ------------------------------------------------------
+
+      AUX1 = NbrToBitS(com_byte, 8);      
+      
+      bts_clone = AUX1[0];
+      bts_clone += AUX1[1];
+      bts_clone += AUX1[2];
+      bts_clone += AUX1[3];
+
+
+
+      if (bts_clone == my_addr)// tem q trocar caso mude o secundário ------------------------------------------------------
       {
-        bts_clone = NbrToBitS(com_byte, 8);
+        
 
-        bts_clone.remove(0, 4);//tira o destinatário
+        AUX1.remove(0, 4);//tira o destinatário
 
-        ult_recebido = bts_clone; // pega o remetente e salva-----------
-        
-        
-        
-        
-        
+        ult_recebido = AUX1; // pega o remetente e salva-----------
+              
         //CONTROLL---------------------
         com_byte = Serial.read();
         bts_clone = NbrToBitS(com_byte, 8);
         delay(1000);
+
+        AUX1 = bts_clone[1];
+        AUX1 += bts_clone[2];
+        AUX1 += bts_clone[3];
+
+        AUX = strToNbr(AUX1);// segura o NS da mensagem
+        
+        ACK = AUX + 1;//incrementa ack1------------------------
+        
 
         //pega o ack põe em AUX
         AUX1 = bts_clone[bts_clone.length() - 3];
         AUX1 += bts_clone[bts_clone.length() - 2];
         AUX1 += bts_clone[bts_clone.length() - 1];
         AUX = strToNbr(AUX1);
-
-        if (AUX > NS) //compara o ack da msg com ns do ultimo frame enviado--------------------
+        
+        
+        if (NS == AUX) //compara o ack da msg com NS do ultimo frame enviado--------------------
         {
-          NS = AUX; //move a fila do buffer------------------
-
-          //AUX1 pega o NS da msg--------------------------
-          AUX1 = bts_clone[1];
-          AUX1 += bts_clone[2];
-          AUX1 += bts_clone[3];
-
-          ACK = strToNbr(AUX1) + 1;//incrementa ack1------------------------
 
           //DADOS --------------------------
           do
@@ -265,24 +292,23 @@ void loop()
           }
           while (com_byte != start_end_Flag);
 
-            ult_recebido.remove((ult_recebido.length() - 8), 8); //arranca a flag final
+          ult_recebido.remove((ult_recebido.length() - 8), 8); //arranca a flag final
 
+          AUX1 = ult_recebido;
+          AUX1.remove(0, 4); //arranca endereço
+            
+          //CRC ---------------------------------------------------------------------------------------
+          CRC = (!(crcCmp(AUX1, DIV)));// mantém 0 quando ta td certo, 1 quando deu ruim
 
-            //CRC ---------------------------------------------------------------------------------------
-            CRC = (!(crcCmp(ult_recebido, DIV)));// mantém 0 quando ta td certo, 1 quando deu ruim
-            if (CRC != 0) ACK--;
+          if (CRC != 0) ACK--;
+
         }
-        else //caso o frame anterior não tenha sido mandado corretamente------------------------------
+        else //caso do ult frame, que este mandou, não tenha sido mandado corretamente----------------------------------
         {
-          //realiza a opr normalmente porém, incrementando apenas o ack e sem armazenar nada
+          Serial.println("ack ruim");
+          //realiza a opr normalmente porém, voltando uma instancia de NS e sem armazenar nada
           //DADOS --------------------------
-          NS = AUX; //move a fila do buffer------------------
-          
-          //AUX1 pega o NS da msg--------------------------
-          AUX1 = bts_clone[1];
-          AUX1 += bts_clone[2];
-          AUX1 += bts_clone[3];
-          ACK = strToNbr(AUX1) + 1;
+          NS = AUX; //volta um NS
 
           do
           {
@@ -296,10 +322,13 @@ void loop()
         }
 
         
-        delay(1000);
-        RX0_TX1 = !RX0_TX1;//volta ao modo falar----------------------------------------
+        delay(1500);
+        RX0_TX1 = !RX0_TX1;//volta ao modo falar---------------------------------------- 
+        Serial.println(NS);
+        Serial.println(ACK);     
       }
     }
+
   }
   
 }
